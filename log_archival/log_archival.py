2c218fe2fd9441efd42c9111db36db5236a9df5d
sashank411 <50548441+sashank411@users.noreply.github.com>
2020-11-18 15:06:06 +0530

    Update log_archival.py

--- log_archival/log_archival.py
+++ log_archival/log_archival.py
@@ -6,12 +6,10 @@ class LogArchival:
 
     def __init__(self):
         self.time=datetime.now()
-        self.year=self.time.strftime("%Y")
-        self.month=self.time.strftime("%m")
-        self.day=self.time.strftime("%d")
+        self.folder_name=self.time.strftime("%Y""%m""%d")
 
 obj=LogArchival()
 time=obj.time
-folder_name="ReachClient_"+str(obj.year)+str(obj.month)+str(obj.day)
-print(time)
+name=obj.folder_name
+folder_name="ReachClient_"+str(name)
 print(folder_name)
