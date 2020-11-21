97b8815ea2315caf87fcb3a97477ba858f413f47
sashank411 <50548441+sashank411@users.noreply.github.com>
2020-11-21 11:48:46 +0530

    Update grab_date_modified.py

--- file_handling/grab_date_modified.py
+++ file_handling/grab_date_modified.py
@@ -1,3 +1,4 @@
 import os.path, time
-print("Last modified: %s" % time.ctime(os.path.getmtime("D:\SetUps\WinDev2010Eval.ova")))
+date_modified = time.ctime(os.path.getmtime("D:\SetUps\WinDev2010Eval.ova"))
+print("Last modified: %s" %date_modified )
 # print("Created: %s" % time.ctime(os.path.getctime("test.txt")))
